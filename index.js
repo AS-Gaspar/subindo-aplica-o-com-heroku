85cc120e40d7ba6f58e53aa71a0eaad7c6805876
AS-Gaspar <airtontunni@gmail.com>
2024-09-26 12:19:38 -0300

    create db

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5001
 
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
 function showTimes() {
   const times = process.env.TIMES || 5
   let result = ''
@@ -20,4 +29,16 @@ express()
    .set('/', (req, res) => res.render('pages/index'))
    .get('/cool', (req, res) => res.send(cool()))
    .get('/times', (req, res) => res.send(showTimes()))
+   .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
