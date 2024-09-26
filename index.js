9103ab07690f8f0938056b237df10d9211060e5b
AS-Gaspar <airtontunni@gmail.com>
2024-09-26 11:18:21 -0300

    Add cool face API

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5001
 
 express()
-   .use(epress.static(path.join(__dirname, 'public')))
+   .use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
    .set('view engine', 'ejs')
    .set('/', (req, res) => res.render('pages/index'))
