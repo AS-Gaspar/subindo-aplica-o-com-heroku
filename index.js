e034c8b8a50c15cbe8df3db06c5c8a3d255fbf11
AS-Gaspar <airtontunni@gmail.com>
2024-09-26 11:14:39 -0300

    change index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 
 const PORT = process.env.PORT || 5001
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+   .use(epress.static(path.join(__dirname, 'public')))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   .set('/', (req, res) => res.render('pages/index'))
+   .get('/cool', (req, res) => res.send(cool()))
+   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
